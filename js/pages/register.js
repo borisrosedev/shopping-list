7834d76bc423f9c9a38645d86517e091b4493590
Boris Rose <boris.alexandre.rose@gmail.com>
2024-04-06 03:18:24 +0200

    feat/fix

--- js/pages/register.js
+++ js/pages/register.js
@@ -1,7 +1,4 @@
-import { LOGOUT_BUTTON, REGISTER_FORM } from "../constants/dom.js";
-import { logoutHandler } from "../handlers/logoutHandler.js";
+import { REGISTER_FORM } from "../constants/dom.js";
 import registerSubmitHandler from "../handlers/registerSubmitHandler.js";
 
-
-LOGOUT_BUTTON.addEventListener("click", logoutHandler)
 REGISTER_FORM.addEventListener("submit", registerSubmitHandler);
--- js/pages/todos.js
+++ js/pages/todos.js
@@ -1,15 +1,17 @@
 import {
   ADD_BUTTON,
+  LOGOUT_BUTTON,
 } from "../constants/dom.js";
 import { createInitiUI } from "../factory/createInitialUI.js";
 import addHandler from "../handlers/addHandler.js";
+import { logoutHandler } from "../handlers/logoutHandler.js";
 
 
 //const pubsubInstance = new PubSubService()
 //pubsubInstance.subscribe("todos")
 //pubsubInstance.message(createNotificationDiv)
 createInitiUI();
-
+LOGOUT_BUTTON.addEventListener("click", logoutHandler)
 ADD_BUTTON.addEventListener("click", addHandler);
  
     
